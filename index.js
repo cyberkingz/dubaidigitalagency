aa930901760f9be2951a1b08a6dedf964af5014f
tonyrcd <tony@marketing-horizon.fr>
2022-05-18 17:57:33 +0400

    rebuild of the website, design css block with CSS Grid Generator

--- index.js
+++ index.js
@@ -1,28 +1,21 @@
-const h1 = document.querySelector("h1");
+// const h1 = document.querySelector("h1");
 
-window.addEventListener(("mousedown"), (e) => {
-        h1.style.color = "red";
-})
 
-window.addEventListener(("mouseup"), (e) => {
-        h1.style.color = "white";
-})
+// h1.addEventListener(("mouseenter"), (e) => {
+//     h1.style.color = "orange";
+//     h1.style.transform = 'rotate(20deg)';
+// })
 
-h1.addEventListener(("mouseenter"), (e) => {
-    h1.style.color = "orange";
-    h1.style.transform = 'rotate(20deg)';
-})
+// h1.addEventListener(("mouseleave"), (e) => {
+//     h1.style.color = "white";
+//     h1.style.transform = 'rotate(0deg)';
+// })
 
-h1.addEventListener(("mouseleave"), (e) => {
-    h1.style.color = "white";
-    h1.style.transform = 'rotate(0deg)';
-})
-
-window.addEventListener(("mousemove"), (e) => {
+// window.addEventListener(("mousemove"), (e) => {
     
-})
+// })
 
 
-/* window.addEventListener(("mousemove"), (e) => {
-    h1.style.color = "red";
-}) */
+// /* window.addEventListener(("mousemove"), (e) => {
+//     h1.style.color = "red";
+// }) */
